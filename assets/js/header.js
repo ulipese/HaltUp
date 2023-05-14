c7b9a457122bc81dd18e270e81039fc6a5702d20
Felipe Sousa <felipe.sousa4030@gmail.com.br>
2023-05-14 18:50:31 -0300

    Add responsive menu and Finish pages About, Contact and Home

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,7 +1,7 @@
 const menu = document.querySelector(".header__menu");
-const nav = document.querySelector(".header__nav")
+const nav = document.querySelector(".header__nav");
 
 menu.addEventListener("click", () => {
   nav.classList.toggle("header__nav--active");
-  console.log("toggle")
+  console.log("toggle");
 });
