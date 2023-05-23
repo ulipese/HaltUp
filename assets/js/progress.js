daac35a31b334578a6ec3c37826a211300f6dfe9
Felipe Sousa <felipe.sousa4030@gmail.com.br>
2023-05-23 19:55:36 -0300

    Add filter to progress.js

--- assets/js/progress.js
+++ assets/js/progress.js
@@ -1,62 +1,87 @@
-function imc(){
-    let masc = document.getElementById("btn-sexo-masc")
-    let fem = document.getElementById("btn-sexo-fem")
-    const inputpeso = document.querySelector(".txt-peso");
-    let peso = Number(inputpeso.value)
-    const inputaltura = document.querySelector(".txt-altura")
-    let altura = Number(inputaltura.value) /100
-    let p = document.querySelector(".result-imc")
-    let resultado = peso / (altura * altura)
+function calcIMC() {
+  const masc = document.getElementById("btn-sexo-masc");
+  const fem = document.getElementById("btn-sexo-fem");
+  const pesoInput = document.querySelector(".txt-peso");
+  const alturaInput = document.querySelector(".txt-altura");
+  const resultInput = document.querySelector(".result-imc");
 
-    if(masc.checked==true){
-        if(resultado > 43){
-            p.textContent = `IMC: ${resultado.toFixed(1)} - Obesidade Morbida`
-        }else if(resultado >= 30){
-            p.textContent = `IMC: ${resultado.toFixed(1)} - Obesidade Moderada`
-        }else if(resultado >= 25){
-            p.textContent = `IMC: ${resultado.toFixed(1)} - Obesidade leve`
-        }else if(resultado >= 20){
-            p.textContent = `IMC: ${resultado.toFixed(1)} - Normal`
-        }else{
-            p.textContent = `IMC: ${resultado.toFixed(1)} - Abaixo do Normal`
-        }
-    }else{
-        if(resultado > 39){
-            p.textContent = `IMC: ${resultado.toFixed(1)} - Obesidade Morbida`
-        }else if(resultado >= 29){
-            p.textContent = `IMC: ${resultado.toFixed(1)} - Obesidade Moderada`
-        }else if(resultado >= 24){
-            p.textContent = `IMC: ${resultado.toFixed(1)} - Obesidade leve`
-        }else if(resultado >= 19){
-            p.textContent = `IMC: ${resultado.toFixed(1)} - Normal`
-        }else{
-            p.textContent = `IMC: ${resultado.toFixed(1)} - Abaixo do Normal`
-        }
+  if (pesoInput.value && alturaInput.value && (fem || masc)) {
+    let peso = Number(pesoInput.value);
+    let altura = Number(alturaInput.value) / 100;
+    let result = peso / (altura * altura);
+    if (masc.checked == true && fem.checked == false) {
+      if (result > 43) {
+        resultInput.textContent = `IMC: ${result.toFixed(
+          1
+        )} - Obesidade Morbida`;
+      } else if (result >= 30) {
+        resultInput.textContent = `IMC: ${result.toFixed(
+          1
+        )} - Obesidade Moderada`;
+      } else if (result >= 25) {
+        resultInput.textContent = `IMC: ${result.toFixed(1)} - Obesidade leve`;
+      } else if (result >= 20) {
+        resultInput.textContent = `IMC: ${result.toFixed(1)} - Normal`;
+      } else {
+        resultInput.textContent = `IMC: ${result.toFixed(
+          1
+        )} - Abaixo do Normal`;
+      }
+    } else {
+      if (result > 39) {
+        resultInput.textContent = `IMC: ${result.toFixed(
+          1
+        )} - Obesidade Morbida`;
+      } else if (result >= 29) {
+        resultInput.textContent = `IMC: ${result.toFixed(
+          1
+        )} - Obesidade Moderada`;
+      } else if (result >= 24) {
+        resultInput.textContent = `IMC: ${result.toFixed(1)} - Obesidade leve`;
+      } else if (result >= 19) {
+        resultInput.textContent = `IMC: ${result.toFixed(1)} - Normal`;
+      } else {
+        resultInput.textContent = `IMC: ${result.toFixed(
+          1
+        )} - Abaixo do Normal`;
+      }
     }
+  } else {
+    resultInput.textContent = "Preencha os campos corretamente!";
+    pesoInput.value ? alturaInput.focus() : pesoInput.focus();
+  }
 }
 
-function clicar(){
-    let hiper = document.getElementById("btn-objetivo_1")
-    let forca = document.getElementById("btn-objetivo_2")
-    let resis = document.getElementById("btn-objetivo_3")
-    const inputcarga = document.querySelector(".txt-carga")
-    let carga = Number(inputcarga.value)
-    const inputreps = document.querySelector(".txt-reps")
-    let reps = Number(inputreps.value)
-    let rm = (0.033 * carga * reps) + carga
-    let p = document.querySelector(".result-rm")
-    let resultado = 0
+function calcRM() {
+  const hiperInput = document.getElementById("btn-objetivo_1");
+  const forcaInput = document.getElementById("btn-objetivo_2");
+  const resisInput = document.getElementById("btn-objetivo_3");
+  const cargaInput = document.querySelector(".txt-carga");
+  const repsInput = document.querySelector(".txt-reps");
+  const resultInput = document.querySelector(".result-rm");
 
-    if(hiper.checked==true){
-        resultado = rm * 0.70
-        p.textContent = `RM: ${rm} - Carga de trabalho: ${resultado}`
-    } else if(forca.checked==true){
-        resultado = rm * 0.85
-        p.textContent = `RM: ${rm} - Carga de trabalho: ${resultado}`
-    }else{
-        resultado = rm * 0.40
-        p.textContent = `RM: ${rm} - Carga de trabalho: ${resultado}`
+  if (
+    cargaInput.value &&
+    repsInput.value &&
+    (hiperInput || forcaInput || resisInput)
+  ) {
+    let carga = Number(cargaInput.value);
+    let reps = Number(repsInput.value);
+    let rm = 0.033 * carga * reps + carga;
+    let result = 0;
+
+    if (hiperInput.checked == true) {
+      result = rm * 0.7;
+      resultInput.textContent = `RM: ${rm} - Carga de trabalho: ${result}`;
+    } else if (forcaInput.checked == true) {
+      result = rm * 0.85;
+      resultInput.textContent = `RM: ${rm} - Carga de trabalho: ${result}`;
+    } else {
+      result = rm * 0.4;
+      resultInput.textContent = `RM: ${rm} - Carga de trabalho: ${result}`;
     }
-
-
+  } else {
+    resultInput.textContent = "Preencha os campos corretamente!";
+    cargaInput.value ? repsInput.focus() : cargaInput.focus();
+  }
 }
